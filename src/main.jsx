b55123fe64878c4ff9f383c33d68cc9e06f0c2a9
FloKrtmr <florian.kreitmeier20@gmail.com>
2026-06-27 20:46:03 +0200

    fix: use HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import L from 'leaflet'
@@ -9,7 +9,7 @@ import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
 L.Icon.Default.mergeOptions({ iconUrl, shadowUrl, iconRetinaUrl: iconUrl })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/race-app">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
